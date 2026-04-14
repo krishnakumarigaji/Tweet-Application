32e96e494a9270f5a507939e7a600dfc4504d038
Krishna Dhakad <krishnadhaker9005@gmail.com>
2026-04-14 11:00:28 +0530

    allowed host

--- chaiheadq/settings.py
+++ chaiheadq/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-lgmwwvw1vt7*a*9yqd6a7jevnf!(a4q04d4-zo_ghtz2gmc+xk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
